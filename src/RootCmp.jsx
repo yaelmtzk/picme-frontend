6b50e867b3bf660eb7575947fa2bd5c16d5b94dc
yaelmtzk <jessianeroque@gmail.com>
2026-01-06 01:01:30 +0200

    Add mobile route

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Routes, Route, useLocation, useNavigate } from 'react-router'
+import { AppHeader } from './cmps/AppHeader.jsx'
 import { StoryIndex } from './pages/StoryIndex.jsx'
 import { StoryDetails } from './pages/StoryDetails.jsx'
 import { UserDetails } from './pages/UserDetails.jsx'
 import { UserMsg } from './cmps/UserMsg.jsx'
 import { LayoutWithNav } from './cmps/LayoutwithNav.jsx'
+import { StoryEntry } from './cmps/StoryEntry.jsx'
 import { addStory } from './store/actions/story.actions.js'
 import { getEmptyStory } from './services/story/index.js'
 import { showSuccessMsg, showErrorMsg } from './services/event-bus.service.js'
@@ -39,14 +41,14 @@ export function RootCmp() {
                     <Route element={<LayoutWithNav onAdd={onAddStory} />}>
                         <Route path="/" element={<StoryIndex />} />
                         <Route path="/:username" element={<UserDetails />} />
-                         <Route path="/p/:id" element={<StoryDetails />} />
+                         <Route path="/p/:id" element={<StoryEntry />} />
                     </Route>
 
                 </Routes>
 
                 {state?.modal && (
                     <Routes>
-                        <Route path="/p/:id" element={<StoryDetails onClose={() => navigate(-1)} />} />
+                        <Route path="/p/:id" element={<StoryEntry onClose={() => navigate(-1)} />} />
                     </Routes>
                 )}
 
